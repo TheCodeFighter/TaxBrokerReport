75b56270ed6063ec68393c8b0e753bd65aadbdbd
David Ogorevc <david.ogorevc0@protonmail.com>
2026-05-14 15:29:50 +0200

    TBR-003: Add workspace naming conventions (#113)
    * TBR-003: Add workspace naming conventions and define types for monetary and trading operations
    
    * TBR-0003: Add fixed-point ratio type and update CorporateAction struct
    
    * TBR-0003: Update copilot instructions to clarify branch and commit naming conventions

---
+++ include/utils/numeric_util.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string_view>
+#include "taxbroker/types_def.hpp"
+
+
+taxbroker::Money parseMoney4(std::string_view value);
+
+taxbroker::Units parseUnits8(std::string_view value);
+
+taxbroker::CorpRatio parseCorpRatio8(std::string_view value);
+
+// To avoid unit64_t overflow when multiplying price and units
+taxbroker::Money multiplyMoneyUnits(taxbroker::Money price, taxbroker::Units units);
